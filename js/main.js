ba110f67a45d18b0b90e7abaac3f8eb1b269d5c7
StefanoDeSa <stefano.b.sa@rede.ulbra.br>
2022-08-16 10:36:47 -0300

    feat(mini-pokedex): shiny pokemon button creation

--- js/main.js
+++ js/main.js
@@ -11,9 +11,11 @@ function buscarPokemon(){
             let img = document.createElement("img")
             let card_body = document.createElement("div")
             let card_text = document.createElement("p")
+            let shiny_button = document.createElement("button")
 
             if(cod != 0){
                 card_pokemon.innerHTML = ''
+                shiny_div.innerHTML = ''
    
             }
 
@@ -28,17 +30,23 @@ function buscarPokemon(){
             
             card_text.innerText = `Nome: ${pokemon.name}\n Id: ${pokemon.id}\n Altura: ${pokemon.height}\n Peso: ${pokemon.weight}`
 
+            shiny_button.className = "btn btn-light"
+            shiny_button.type = "button"
+            shiny_button.innerText = "Shiny"
+            shiny_button.setAttribute("onclick", "mudarShiny()")
 
             card_body.appendChild(card_text)
             card.appendChild(img)
             card.appendChild(card_body)
             card_pokemon.appendChild(card)
+            shiny_div.appendChild(shiny_button)
             cod = 1
         })
     })
 }
 function buscarPokemon_lista(url){
     let card_pokemon = document.getElementById('card_pokemon')
+    let shiny_div = document.getElementById('shiny_div')
     document.getElementById('pokemon').value='';
     fetch(url)
     .then(function(response){
@@ -47,9 +55,11 @@ function buscarPokemon_lista(url){
             let img = document.createElement("img")
             let card_body = document.createElement("div")
             let card_text = document.createElement("p")
+            let shiny_button = document.createElement("button")
 
             if(cod != 0){
                 card_pokemon.innerHTML = ''
+                shiny_div.innerHTML = ''
    
             }
 
@@ -64,11 +74,16 @@ function buscarPokemon_lista(url){
             
             card_text.innerText = `Nome: ${pokemon.name}\n Id: ${pokemon.id}\n Altura: ${pokemon.height}\n Peso: ${pokemon.weight}`
 
+            shiny_button.className = "btn btn-light"
+            shiny_button.type = "button"
+            shiny_button.innerText = "Shiny"
+            shiny_button.setAttribute("onclick", "mudarShiny()")
 
             card_body.appendChild(card_text)
             card.appendChild(img)
             card.appendChild(card_body)
             card_pokemon.appendChild(card)
+            shiny_div.appendChild(shiny_button)
             cod = 1
         })
     })
@@ -100,4 +115,18 @@ function listaPokemon(){
             cod = 1
         })
     })
+}
+let is_shiny = "n"
+function mudarShiny(){
+    let card_pokemon = document.getElementsByClassName("card-img-top")
+    if(card_pokemon[0].src.includes("other") || is_shiny == "n"){
+        url_slice = card_pokemon[0].src.split("other/official-artwork/")
+        url = url_slice[0]+"shiny/"+url_slice[1]  
+        card_pokemon[0].src = url
+        is_shiny = "y"
+    }
+    else{
+        alert("Pokemon já se encontra na versão shiny")
+    }
+    
 }
